dc3e0c5ce5bf985947612ddfd6581f009f2d13b8
Keerthana_S <keerthana05117@gmail.com>
2026-02-06 04:29:09 +0000

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,1 +1,13 @@
-
+<?php
+$ip = $_SERVER['SERVER_ADDR'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Streamline v1</title>
+</head>
+<body>
+  <h1>Welcome to Streamline - v1</h1>
+  <p>Server IP: <?php echo $ip; ?></p>
+</body>
+</html>
