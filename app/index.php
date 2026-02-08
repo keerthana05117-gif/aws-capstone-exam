47318ee918952eb6b858ecb0dfa6b4471ea6c0d7
Keerthana_S <keerthana05117@gmail.com>
2026-02-08 11:31:06 +0000

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,4 +1,21 @@
 <?php
-echo "<h1>Welcome to Streamline - v1</h1>";
-echo "<p>Server IP: " . $_SERVER['SERVER_ADDR'] . "</p>";
+$ip = $_SERVER['SERVER_ADDR'];
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Streamline v2</title>
+    <style>
+        body {
+            background-color: #e0f7fa;
+            font-family: Arial;
+            text-align: center;
+            padding-top: 100px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Welcome to StreamLine - v2 [New Feature]</h1>
+    <p>Server IP: <?php echo $ip; ?></p>
+</body>
+</html>
