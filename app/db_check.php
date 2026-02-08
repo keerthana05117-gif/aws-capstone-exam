d1e4881b0220c81393b7a529c566208e99e40f1a
Keerthana_S <keerthana05117@gmail.com>
2026-02-08 07:27:54 +0000

    Update db_check.php

--- app/db_check.php
+++ app/db_check.php
@@ -1,1 +1,14 @@
+<?php
+$host = "RDS_ENDPOINT_HERE";
+$user = "admin";
+$pass = "password123";
+
+$conn = mysqli_connect($host, $user, $pass);
+
+if ($conn) {
+    echo "Database Connected Successfully";
+} else {
+    echo "Database Connection Failed";
+}
+?>
 
