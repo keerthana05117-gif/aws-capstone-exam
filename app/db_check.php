003c2170892b1a6de692ef07883746e63d68acc9
Keerthana_S <keerthana05117@gmail.com>
2026-02-08 08:02:49 +0000

    Update db_check.php

--- app/db_check.php
+++ app/db_check.php
@@ -1,7 +1,7 @@
 <?php
 $host = "terraform-20260208073718626400000004.c162o0mie7ts.us-east-1.rds.amazonaws.com";
 $user = "admin";
-$pass = "password123";
+$pass = "Keert17@";
 
 $conn = mysqli_connect($host, $user, $pass);
 
